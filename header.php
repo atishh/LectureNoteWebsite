4b991e989113bce79898775e93abf06371e10a6f
atish <7705506+atishh@users.noreply.github.com>
2017-01-01 20:43:54 +0530

    Conditional logic, Template file with matching name slug or id, custom page template

--- header.php
+++ header.php
@@ -14,15 +14,19 @@
         <!-- site-header -->
         <header class="site-header">
             <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            <h5><?php bloginfo('description'); ?></h5>
+            <h5><?php bloginfo('description'); ?>
+                <?php if (is_page('portfolio')) { ?>
+                    Thank You for visiting site.
+                <?php } ?>
 
-            <?php
-            $args = array(
-                'theme_location' => 'primary'
-            );
-            ?>
-
+            </h5>
+            
             <nav class="site-nav">
+                <?php
+                $args = array(
+                    'theme_location' => 'primary'
+                );
+                ?>
                 <?php wp_nav_menu( $args ); ?>
             </nav>
 
