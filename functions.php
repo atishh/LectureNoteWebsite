fb69ac3f94fbfd58833d77c354c7af2e67001cc7
atish <7705506+atishh@users.noreply.github.com>
2017-01-03 03:58:49 +0530

    Adding featured image in the post

--- functions.php
+++ functions.php
@@ -7,12 +7,6 @@ function LNSite_resources()
 
 add_action('wp_enqueue_scripts', 'LNSite_resources');
 
-//Navigation Menus
-register_nav_menus(array(
-    'primary' => __( 'Primary Menu'),
-    'footer' => __( 'Footer Munu'),
-));
-
 // Get top ancestor
 function get_top_ancestor_id() 
 {
@@ -39,3 +33,20 @@ function custom_excerpt_length() {
 }
 
 add_filter('excerpt_length', 'custom_excerpt_length');
+
+// Theme setup
+function LNSite_setup() 
+{
+    //Navigation Menus
+    register_nav_menus(array(
+        'primary' => __( 'Primary Menu'),
+        'footer' => __( 'Footer Munu'),
+    ));
+
+    // Add featured image support
+    add_theme_support('post-thumbnails');
+    add_image_size('small-thumbnail', 180, 120, true);
+    add_image_size('banner-image', 920, 210, true);
+}
+
+add_action('after_setup_theme', 'LNSite_setup');
--- single.php
+++ single.php
@@ -24,7 +24,9 @@ if (have_posts()):
         }
         ?>
         
-        </p>        
+        </p>
+        
+        <?php the_post_thumbnail('banner-image'); ?>        
 
         <?php the_content(); ?>
     </article>
