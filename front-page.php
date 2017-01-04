e6645f35bfc4130d81a5450c6d7b5c0f045ada74
atish <7705506+atishh@users.noreply.github.com>
2017-01-04 19:16:36 +0530

    Custom loop WP_Query

--- front-page.php
+++ front-page.php
@@ -38,8 +38,51 @@ if (have_posts()):
 else :
     echo '<p>No content found</p>';
 
-endif;
+endif; ?>
 
+<div class="home-columns clearfix">
+
+    <div class="one-half">
+        <?php // opinion posts loop begins here
+        $opinionPosts = new WP_Query('cat=10&posts_per_page=2&orderby=title&order=ASC');
+        
+        if ($opinionPosts->have_posts()) :
+        
+            while ($opinionPosts->have_posts()) : $opinionPosts->the_post();
+                ?>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+            <?php endwhile;  
+            
+            else :
+                //fallback no content message
+        endif;
+        wp_reset_postdata(); ?>
+
+    </div>
+
+    <div class="one-half last">
+        <?php // news posts loop begins here
+        $newsPosts = new WP_Query('cat=1&posts_per_page=2&orderby=title&order=ASC');
+        
+        if ($newsPosts->have_posts()) :
+        
+            while ($newsPosts->have_posts()) : $newsPosts->the_post();
+                ?>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <?php the_excerpt(); ?>
+            <?php endwhile;  
+            
+            else :
+                //fallback no content message
+        endif;
+        wp_reset_postdata(); ?>
+
+    </div>
+
+</div>
+
+<?php
 get_footer();
 
 ?>
